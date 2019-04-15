87894fcc131d4973a40bc92b4d609de637e22b0c
Jesse Howe <jchowe93@gmail.com>
2019-04-15 18:13:06 -0500

    - attempt to fix diff issues

--- cleaning-data.R
+++ cleaning-data.R
@@ -16,19 +16,21 @@ KH1_raw <- gsub("let’s continue. The ingredients for a heart:","let’s contin
 KH1_raw <- gsub("Realize the destiny:","Realize the destiny,",KH1_raw)
 
 # Remove Header and Footer from Script
-KH1_start <- str_which(KH1_raw, fixed("????: I've been having these weird thoughts lately... Like, is any of this"))
+KH1_start <- str_which(KH1_raw, fixed("Sora: I've been having these weird thoughts lately... Like, is any of this"))
 KH1_end <- str_which(KH1_raw, fixed("Everything is coming back to me, the true..."))
 
 # Subset script
 KH1_sub <- KH1_raw[(KH1_start): (KH1_end)]
 
 
-KH1 <- data_frame(KH1_sub) %>%
+KH1 <- data_frame(KH1_sub = KH1_sub) %>%
   # Filtering out empty lines
   filter(KH1_sub != "") %>%
+  mutate(level = str_detect(KH1_sub, "\\["),
+         scene = cumsum(level)) %>%
   # Seperating characters from their lines, fill left will allow for lines broken onto new lines to be merged. (This also merges character actions in parentheses to be merged into dialogue, we'll deal with this later on.)
   separate(KH1_sub, c("speaker", "dialogue"), sep = ":", fill = "left") %>%
-  group_by(line = cumsum(!is.na(speaker))) %>%
+  group_by(scene, line = cumsum(!is.na(speaker))) %>%
   summarize(speaker = speaker[1], dialogue = str_c(dialogue, collapse = " "))
 
 # Now it's time to remove the actions in parentheses
@@ -38,5 +40,3 @@ KH1$speaker <- gsub("\\s*\\([^\\)]+\\)","",KH1$speaker)
 
 #Let's see what have now
 unique(KH1$speaker)
-
-# It appears that the script denotes different mysterious characters with a different number of question marks, let's correct those
